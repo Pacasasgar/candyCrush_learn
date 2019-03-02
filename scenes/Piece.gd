20c38675cff0dfac6992d40085e093f342cc25e4
Pau Casas <pacasasgar@gmail.com>
2019-03-02 12:14:05 +0100

    Videos 6, 7 y 8. Swap pieces.

--- scenes/Piece.gd
+++ scenes/Piece.gd
@@ -2,7 +2,12 @@ extends Node2D
 
 export (String) var color
 
+var move_tween
+
 func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
-	pass
+	move_tween = get_node("move_tween")
+	#move_tween = &move_tween
+
+func move(target):
+	move_tween.interpolate_property(self, "position", position, target, .3, Tween.TRANS_ELASTIC, Tween.EASE_OUT)
+	move_tween.start()
