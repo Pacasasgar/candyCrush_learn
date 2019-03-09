988d2fbeb61d9ea29ffedb772711eab5c9b8e63c
Pau Casas <pacasasgar@gmail.com>
2019-03-09 12:35:09 +0100

    Matching + Destroying pieces

--- piece.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-export (String) var color
-
-func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
-	pass
--- scenes/Piece.gd
+++ scenes/Piece.gd
@@ -3,6 +3,7 @@ extends Node2D
 export (String) var color
 
 var move_tween
+var matched = false
 
 func _ready():
 	move_tween = get_node("move_tween")
@@ -11,3 +12,7 @@ func _ready():
 func move(target):
 	move_tween.interpolate_property(self, "position", position, target, .3, Tween.TRANS_ELASTIC, Tween.EASE_OUT)
 	move_tween.start()
+
+func dim():
+	var sprite = get_node("Sprite")
+	sprite.modulate = Color(1, 1, 1, .5)
